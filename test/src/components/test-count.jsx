fab74956565d0af551b516a9edf8111919653fa0
TheHaive <vadim.adiyatov@gmail.com>
2023-06-20 20:12:07 +0500

    изменил подпись кнопки

--- test/src/components/test-count.jsx
+++ test/src/components/test-count.jsx
@@ -7,7 +7,7 @@ const TestCount = () => {
 
     return <div>
         <h1>{count}</h1>
-        <button onClick={() => setCount(count + 1)}>Клик</button>
+        <button onClick={() => setCount(count + 1)}>НАЖМИ НА МЕНЯ</button>
     </div>
 }
 
